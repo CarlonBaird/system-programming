a114db573f52a4d7d1baa00d08189da7374cf8a8
wi-cuckoo <yp_liuwei@163.com>
2015-07-26 09:09:27 +0800

    v0.1

--- part1-file/scan.c
+++ part1-file/scan.c
@@ -14,19 +14,67 @@
 //#include<getopt.h>  //for supporting long option looks like --help
 #include<errno.h>
 
+void dof_read( int _fd)
+{
+	ssize_t ret;
+	int total_byte = 0;
+	char buf[1025];
+	while((ret = read(_fd, buf,1024)) > 0)
+	{
+		if(ret == -1)
+		{
+			if(errno == EINTR)
+				continue;
+			perror("read failed");
+			break;
+		}
+		total_byte += ret;
+		printf("%s", buf);
+		memset(buf, 0, 1024);      //this is a important operation to blush buf
+	}
+	printf("\nthe total bytes is: %d \n", total_byte);
+
+}
+
+void dof_write(int _fd)
+{
+	const char *buf = "fuck the programming\n";
+	ssize_t nr;
+
+	nr = write(_fd, buf,strlen(buf));
+	if(nr == -1)
+	// error 
+	{
+		perror("write failed");
+	}
+
+}
 int main(int argc, char *argv[])
 {
 
 	char *filename;
+	char flag;
 //get parameters from terminal
 	int ch;
 	opterr = 0;
-	while((ch = getopt(argc, argv, "c:sh")) != -1)
+	while((ch = getopt(argc, argv, "c:s:w:h")) != -1)
 	{
 		switch(ch)
 		{
-			case 'c': strcpy(filename, optarg); break;
-			default:; 
+			case 'c': 
+				flag = 'c';
+				strcpy(filename, optarg);
+			   	break;
+			case 's': 
+				flag = 's';
+			   	strcpy(filename, optarg);
+			   	break;
+			case 'w':
+			   	flag = 'w';
+			   	strcpy(filename, optarg);
+			   	break;
+//			case 'h': flag = 'h'; break;
+//			default :; 
 		}
 	}
 
@@ -34,41 +82,22 @@ int main(int argc, char *argv[])
 	if(argv[optind] != NULL)
 		strncpy(filename, argv[optind], sizeof(argv[optind])-1);
 
-//	printf("%s\n",filename);
+	printf("%s\n",filename);
 //	open a file, if no exist, creat it
 	int fd;
-	fd = open(filename, O_RDONLY|O_NONBLOCK);
+	fd = open(filename, O_RDWR|O_CREAT|O_NONBLOCK, 0644);
 	if(fd == -1)
 	{
 		//error info ouput
 		perror("wtf");
+		return 1;
 	};
 
 //read a file
-
-	ssize_t ret;
-	int total_byte;
-	char buf[1024];
-	while((ret = read(fd, buf,1024)) > 0)
-	{
-		total_byte += ret;
-		printf("%s", buf);
-		memset(buf, 0, 1024);      //this is a important operation to blush buf
-	}
-	printf("\n%d", total_byte);
-
-/*
-// write into a file
-	const char *buf = "fuck the programming\n";
-	ssize_t nr;
-
-	nr = write(fd, buf,strlen(buf));
-	if(nr == -1)
-	// error 
-	{
-		perror("write failed");
-	}
-*/
+	if(flag == 's')
+		dof_read(fd);
+//write to a file
+	
 	fdatasync(fd);
 
 	if(close(fd) == -1)
@@ -76,3 +105,4 @@ int main(int argc, char *argv[])
 	
 	return 0;
 }
+
